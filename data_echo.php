2fe7e5584423e61f9dd4d661287b654938cfa268
Sunny Singh <sunny@doeity.com>
2019-01-27 17:38:54 +0530

    Create data_echo.php

---
+++ data_echo.php
@@ -0,0 +1,10 @@
+<?php 
+$namm = $_GET["nam"];
+$agee = $_GET["age"];
+$phonee = $_GET["phone"];
+
+echo "Your name is $namm <br>";
+echo "Your age is $agee <br>";
+echo "Your phone is $phonee <br>";
+
+?>
